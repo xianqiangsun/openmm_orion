ce23a39c9478bc72194b73877fed5a97f2d7fec2
Jharrod LaFon <jlafon@eyesopen.com>
2016-12-14 10:46:19 -0700

    Use standard library lzma for compression. Using the included test data, system.xml goes from ~300MB to 3.5MB

--- OpenMMCubes/ports.py
+++ OpenMMCubes/ports.py
@@ -1,18 +1,18 @@
 from __future__ import unicode_literals
-"""
-"""
-import struct
 from floe.api import OutputPort, InputPort
 from floe.constants import BYTES
 from simtk import openmm, unit
+from lzma import compress, decompress
+
 
 class OpenMMSystemSerializationMixin(object):
 
     def encode(self, openmm_system):
-        return openmm.XmlSerializer.serialize(openmm_system).encode()
+        return compress(openmm.XmlSerializer.serialize(openmm_system).encode())
 
     def decode(self, serialized_system):
-        return openmm.XmlSerializer.deserialize(serialized_system.decode())
+        return decompress(openmm.XmlSerializer.deserialize(serialized_system.decode()))
+
 
 class OpenMMSystemInput(OpenMMSystemSerializationMixin, InputPort):
     PORT_TYPES = (BYTES, "OpenMMSystem")
