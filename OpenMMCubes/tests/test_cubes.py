50ec58beed45601adfe9913b44309dfeab3581b6
Nathan Lim <limn1@uci.edu>
2017-01-18 19:08:27 -0800

    Add simulation cube tester and input files

--- OpenMMCubes/tests/test_cubes.py
+++ OpenMMCubes/tests/test_cubes.py
@@ -1,6 +1,6 @@
 import unittest
 
-from OpenMMCubes.cubes import OpenMMComplexSetup
+from OpenMMCubes.cubes import OpenMMComplexSetup, OpenMMSimulation
 from OpenMMCubes.ports import OpenMMSystemInput, OpenMMSystemOutput
 from simtk import openmm, unit
 
@@ -9,13 +9,13 @@ from floe.test import CubeTestRunner
 from openeye import oechem
 
 
-class SleepyCubeTester(unittest.TestCase):
+class SetupCubeTester(unittest.TestCase):
     """
-    Example test of an individual cube
+    Test the OpenMM complex setup cube
     """
 
     def setUp(self):
-        self.cube = OpenMMComplexSetup("sleepy")
+        self.cube = OpenMMComplexSetup("complex_setup")
         self.cube.args.protein = 'input/T4-protein.pdb'
         self.cube.args.ligand = 'input/toluene.pdb'
         self.cube.args.molecule_forcefield = 'input/forcefield/smirff99Frosst.ffxml'
@@ -28,11 +28,9 @@ class SleepyCubeTester(unittest.TestCase):
     def test_success(self):
         # Read a molecule
         mol = oechem.OEMol()
-        pdbfilename = self.cube.args.protein
-        #pdbfilename = 'OpenMMCubes/tests/input/toluene.pdb'
-        ifs = oechem.oemolistream(pdbfilename)
+        ifs = oechem.oemolistream(self.cube.args.ligand)
         if not oechem.OEReadMolecule(ifs, mol):
-            raise Exception('Cannot read molecule from %s' % pdbfilename)
+            raise Exception('Cannot read molecule from %s' % self.cube.args.ligand)
         ifs.close()
 
         # Process the molecules
@@ -56,6 +54,35 @@ class SleepyCubeTester(unittest.TestCase):
         decoded = intake.decode(encoded)
 
         assert (decoded.getNumParticles() == 0)
+
+
+
+class SimulationCubeTester(unittest.TestCase):
+
+    def setUp(self):
+        self.cube = OpenMMSimulation("md_sim")
+        self.runner = CubeTestRunner(self.cube)
+        self.cube.args.complex_pdb = 'input/combined_structure.pdb'
+        self.cube.args.system = 'input/system.xml.xz'
+        self.runner = CubeTestRunner(self.cube)
+        self.runner.start()
+
+    def tearDown(self):
+        self.runner.finalize()
+
+    def test_success(self):
+        mol = oechem.OEMol()
+        ifs = oechem.oemolistream(self.cube.args.complex_pdb)
+        if not oechem.OEReadMolecule(ifs, mol):
+            raise Exception('Cannot read molecule from %s' % self.cube.args.complex_pdb)
+        ifs.close()
+        self.cube.process(mol, self.cube.intake.name)
+        state = self.runner.outputs["checkpoint"].get()
+
+        self.assertIs(type(state), openmm.State)
+
+    def test_failure(self):
+        pass
 
 if __name__ == "__main__":
         unittest.main()
