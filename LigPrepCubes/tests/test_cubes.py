e9b518acbc45787034d31639f0caba37e02c52a5
davidlmobley <dmobley@gmail.com>
2017-03-02 13:26:00 -0800

    Fix test of GAFF parameterization

--- LigPrepCubes/tests/test_cubes.py
+++ LigPrepCubes/tests/test_cubes.py
@@ -2,7 +2,7 @@ import unittest, parmed, base64, pickle
 from LigPrepCubes.ports import (CustomMoleculeInputPort, CustomMoleculeOutputPort)
 from LigPrepCubes.omega import OEOmegaConfGen
 from LigPrepCubes.oedock import FREDDocking
-from LigPrepCubes.cubes import SMIRFFParameterization, SetIDTagfromTitle, OEBSinkCube
+from LigPrepCubes.cubes import SMIRFFParameterization, SetIDTagfromTitle, OEBSinkCube, GAFFParameterization
 from OpenMMCubes.utils import download_dataset_to_file, get_data_filename
 from OpenMMCubes.cubes import OpenMMComplexSetup, OpenMMSimulation
 from OpenMMCubes.ports import ( ParmEdStructureInput, ParmEdStructureOutput,
@@ -95,6 +95,65 @@ class SMIRFFTester(unittest.TestCase):
     def setUp(self):
         self.cube = SMIRFFParameterization('smirff')
         self.cube.args.molecule_forcefield = 'smirff99Frosst.ffxml'
+        self.runner = CubeTestRunner(self.cube)
+        self.runner.start()
+
+    def test_success(self):
+        print('Testing cube:', self.cube.name)
+        self.cube.args.ligand = get_data_filename('toluene.pdb')
+        # Read a molecule
+        mol = oechem.OEMol()
+        ifs = oechem.oemolistream(self.cube.args.ligand)
+        if not oechem.OEReadMolecule(ifs, mol):
+            raise Exception('Cannot read molecule from %s' % self.cube.args.ligand)
+        ifs.close()
+
+        # Process the molecules
+        self.cube.process(mol, self.cube.intake.name)
+
+        # Assert that one molecule was emitted on the success port
+        self.assertEqual(self.runner.outputs['success'].qsize(), 1)
+        # Assert that zero molecules were emitted on the failure port
+        self.assertEqual(self.runner.outputs['failure'].qsize(), 0)
+
+        # Get the output molecule
+        outmol = self.runner.outputs["success"].get()
+
+        # Check for the OpenMM System
+        serialized_system = outmol.GetData(oechem.OEGetTag('system'))
+        # System should be encoded to str on output
+        self.assertIsInstance(serialized_system, str)
+        # Check it can regenerate System
+        system = openmm.XmlSerializer.deserialize(serialized_system)
+        self.assertIsInstance(system, openmm.System)
+        self.assertEqual(system.getNumParticles(),mol.NumAtoms())
+
+        # Check for the ParmEd Structure
+        encoded_structure = outmol.GetData(oechem.OEGetTag('structure'))
+        self.assertIsInstance(encoded_structure, str)
+        decoded_structure = base64.b64decode(encoded_structure)
+        struct_dict = pickle.loads(decoded_structure)
+        struct = parmed.structure.Structure()
+        struct.__setstate__(struct_dict)
+        self.assertEqual(len(struct.atoms),mol.NumAtoms())
+        print(struct)
+        struct_string = [x.strip() for x in str(struct).split(';')]
+        self.assertIn('parametrized>', struct_string)
+
+    def test_failure(self):
+        pass
+
+    def tearDown(self):
+        self.runner.finalize()
+
+class GAFFTester(unittest.TestCase):
+    """
+    Test the GAFF Parameterization cube
+    Example inputs from `openmm_orion/examples/data`
+    """
+    def setUp(self):
+        self.cube = GAFFParameterization('gaff')
+        self.cube.args.molecule_forcefield = 'GAFF2'
         self.runner = CubeTestRunner(self.cube)
         self.runner.start()
 
