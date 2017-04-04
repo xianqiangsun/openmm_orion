a59be2f8ea24f7b50b576c1e615ba8289a87109e
bayly <bayly@eyesopen.com>
2017-04-03 18:41:26 -0600

    method "begin" in the OpenMMComplexSetup class no longer writes a pdb file to read into an OpenMM object; instead it reads the input protein pdb directly. This fixes a parallelism race condition that was causing errors.

--- OpenMMCubes/cubes.py
+++ OpenMMCubes/cubes.py
@@ -1,4 +1,5 @@
 import io, os, traceback
+import uuid
 import numpy as np
 import mdtraj, parmed
 from openeye import oechem
@@ -72,21 +73,23 @@ class OpenMMComplexSetup(ParallelOEMolComputeCube):
         help_text='Forcefield parameters for solvent')
 
     def begin(self):
-        pdbfilename = 'protein.pdb'
+        #pdbfilename = 'protein.pdb'
         protein = oechem.OEMol()
         self.args.protein = utils.download_dataset_to_file(self.args.protein)
+        # Read the PDB file into an OEMol
         with oechem.oemolistream(self.args.protein) as ifs:
             if not oechem.OEReadMolecule(ifs, protein):
                 raise RuntimeError("Error reading protein")
-        with oechem.oemolostream(pdbfilename) as ofs:
-            res = oechem.OEWriteConstMolecule(ofs, protein)
-            if res != oechem.OEWriteMolReturnCode_Success:
-                raise RuntimeError("Error writing protein: {}".format(res))
+        #with oechem.oemolostream(pdbfilename) as ofs:
+        #    res = oechem.OEWriteConstMolecule(ofs, protein)
+        #    if res != oechem.OEWriteMolReturnCode_Success:
+        #        raise RuntimeError("Error writing protein: {}".format(res))
 
         # Read the PDB file into an OpenMM PDBFile object
-        self.proteinpdb = app.PDBFile(pdbfilename)
-        if self.proteinpdb:
-            utils.cleanup(['protein.pdb'])
+        #self.proteinpdb = app.PDBFile(pdbfilename)
+        self.proteinpdb = app.PDBFile(self.args.protein)
+        #if self.proteinpdb:
+        #    utils.cleanup(['protein.pdb'])
         self.opt = vars(self.args)
         self.opt['Logger'] = self.log
 
