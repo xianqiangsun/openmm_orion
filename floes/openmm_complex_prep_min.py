306f8e9b474fecd6a7858a4d2efdf24af247af79
Gaetano Calabro <gcalabro@eyesopen.com>
2017-08-27 11:59:25 -0600

    added a flow to assemble the complex and minimize it

---
+++ floes/openmm_complex_prep_min.py
@@ -0,0 +1,72 @@
+from __future__ import unicode_literals
+from floe.api import WorkFloe, OEMolIStreamCube, OEMolOStreamCube
+from ComplexPrepCubes.cubes import ProteinReader, SolvationCube, ComplexPrep, ForceFieldPrep
+from OpenMMCubes.cubes import  OpenMMminimizeCube
+from LigPrepCubes.cubes import LigChargeCube
+
+job = WorkFloe("ComplexPrepMin")
+
+job.description = """
+Complex Preparation and Minimization Workflow
+
+Ex. python floes/openmm_complex_prep.py --protein protein.oeb
+--ligands ligands.oeb  --ofs-data_out min.oeb
+
+Parameters:
+-----------
+protein (file): OEB file of the prepared protein
+ligands (file): OEB file of the prepared ligands
+
+
+Outputs:
+--------
+ofs: Output file of the assembled and minimized complex
+"""
+
+job.classification = [['Simulation']]
+job.tags = [tag for lists in job.classification for tag in lists]
+
+# Ligand setting
+iligs = OEMolIStreamCube("Ligands", title="Ligand Reader")
+iligs.promote_parameter("data_in", promoted_name="ligands", title="Ligand Input File", description="Ligand file name")
+
+chargelig = LigChargeCube("LigCharge")
+chargelig.promote_parameter('max_conformers', promoted_name='max_conformers',
+                            description="Set the max number of conformers per ligand", default=800)
+
+# Protein Setting
+iprot = ProteinReader("ProteinReader")
+iprot.promote_parameter("data_in", promoted_name="protein", title="Protein Input File", description="Protein file name")
+iprot.promote_parameter("protein_prefix", promoted_name="protein_prefix", default='PRT',
+                        description="Protein Prefix")
+
+solvate = SolvationCube("Solvation")
+
+# Complex Setting
+complx = ComplexPrep("Complex")
+ff = ForceFieldPrep("ForceField")
+
+# Minimization
+minComplex = OpenMMminimizeCube('minComplex')
+minComplex.promote_parameter('steps', promoted_name='steps')
+
+ofs = OEMolOStreamCube('ofs', title='OFS-Success')
+ofs.set_parameters(backend='s3')
+
+fail = OEMolOStreamCube('fail', title='OFS-Failure')
+fail.set_parameters(backend='s3')
+fail.set_parameters(data_out='fail.oeb.gz')
+
+job.add_cubes(iprot, solvate, iligs, chargelig, complx, ff, minComplex, ofs, fail)
+
+iprot.success.connect(solvate.intake)
+solvate.success.connect(complx.system_port)
+iligs.success.connect(chargelig.intake)
+chargelig.success.connect(complx.intake)
+complx.success.connect(ff.intake)
+ff.success.connect(minComplex.intake)
+minComplex.success.connect(ofs.intake)
+minComplex.failure.connect(fail.intake)
+
+if __name__ == "__main__":
+    job.run()
